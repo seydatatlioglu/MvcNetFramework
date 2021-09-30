80ee75da1dc537f85378ec19034d6fd6ede6c7c4
seydatatlioglu <statlioglu0@gmail.com>
2021-09-30 17:13:51 +0300

    admin add

---
+++ BusinessLayer/Abstract/IAdminService.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAdminService
+    {
+        List<Admin> GetList();
+        void AdminAdd(Admin admin);
+        void AdminDelete(Admin admin);
+        void AdminUpdate(Admin admin);
+        Admin GetById(int id);
+    }
+}
