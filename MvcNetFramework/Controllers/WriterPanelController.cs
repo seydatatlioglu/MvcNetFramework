6399aa8f58a167a1519faa8fa4a4bc0e2cdd1e65
seydatatlioglu <statlioglu0@gmail.com>
2021-10-06 10:35:54 +0300

    writer panel

--- MvcNetFramework/Controllers/WriterPanelController.cs
+++ MvcNetFramework/Controllers/WriterPanelController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,41 @@ namespace MvcNetFramework.Controllers
     public class WriterPanelController : Controller
     {
         // GET: WriterPanel
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public ActionResult WriterProfile()
         {
             return View();
         }
+
+        public ActionResult MyHeading(int id)
+        {
+            
+            var headingValue = headingManager.GetListByWriter(id);
+            return View(headingValue);
+        }
+
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in categoryManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            headingManager.HeadingAdd(heading);
+            return RedirectToAction("Index");
+
+        }
+
+
     }
 }
